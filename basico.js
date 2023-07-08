bd318cb3f616532c52f38c4724324fa547916894
Marcelo Petroni <marceloapetroni@hotmail.com>
2023-07-08 16:49:39 -0300

    log, object, array

--- basico.js
+++ basico.js
@@ -1,6 +1,27 @@
 // COMANDOS BÁSICOS JS
+// é uma linguagem não tipada, interpretada, no qual pode declarar variáveis sem definir um tipo.
 
 // LEITURA DE DADOS:
 console.log("Algo para exibir.");
-// é usado aspas simples para strings longas com mais de uma linha;
+// sempre finalizar linhas/bloco de código com ponto vírgula;
+// é usado aspas simples para strings longas com mais de uma linha.
 
+// undefined vs null: null é um objeto vazio que existe, já undefined não existe.
+
+// OBJETO:
+// declarado através de uma chaves {}
+
+var objetoVazio = {};
+var objeto = {
+    name: "Marcelo", // separa os atributos/propriedades por vírgula
+    sobrenone:"Nunes",
+    idade: 15
+};
+
+console.log(objeto.name);
+
+// ARRAY:
+// declarado a partir de colchetes []
+
+var array = [primeiroElemento = "leite", segundoElemento = "ovos"];
+console.log(array);
