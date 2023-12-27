22653cb3d6a9b9dafaabe8f0595629d6a1effffc
marcelopetroni <marceloapetroni@hotmail.com>
2023-12-27 14:03:21 -0300

    CRUD no express

--- express.js
+++ express.js
@@ -2,6 +2,15 @@
     // é um framework do node.js  que facilitam o desenvolvimento de aplicações web e API Rest
     // Roteamento: Permite mapear URL's para requisições
 
+    // Utilize em conjunto a biblioteca nodemon que reinicia automaticamente o servidor sempre 
+    // que detecta uma alteração no código. escreva no terminal: npm install -g nodemon
+    // snippet para inicializar biblioteca:
+    /* em package.json do projeto escreva: "scripts": {
+                                                "dev": "nodemon index.js" 
+                                                // assim podes usar npm run dev qu nem no front-end para iniciar
+                                            }
+    */
+    // PROJETO NODE EXPRESS
     // primeiro é preciso baixar o express em seu projeto por: npm install express
 
     const express = require('express');
@@ -16,31 +25,98 @@
         const PORT = 3000;
         app.listen(PORT);
 
-// exemplo de CRUD (olhar documentação node):
+    // QUER CONECTAR BACK-END COM BANCO DE DADOS? veja um exemplo pelo MySQL:
+        // primeiro importe o banco, nesse caso o MySQL:
+        const mysql = require("mysql");
 
-// GET (ler/aquirir)
+        // faça a ponte por:
+        const db = mysql.createPool({ // Quando uma aplicação precisa se conectar ao banco de dados, 
+            host: "localhost",        // ela obtém uma conexão do pool.
+            user: "root",
+            password: "escolheUma",
+            database: "NomeDaTabela"
+        })
 
-server.get('/rotaDesejada', (req, res) => {
-    // req é de requisição recebida de outra aplicação
-    // res é de response que é a resposta enviada para outra aplicação
-    res.send('Hello world!');
-        // send para enviar dado.
-})
+// QUERY 
+    // Query essencialmente usado para consultas CRUD a partir de um código SQL.
+    // veja:
+        coneccaoBanco.query(sql, (error, results) => { // a conecção é o db declarado acima.
+        if (error) {                                   // veja em POST como é utilizado isso.
+          console.error(error);
+        }})
+        // se houver um erro, ele será exibido, se não, a consulta foi realizada com sucesso.
+        // o uso de query é importante para segurança, pois define dentro de seu escopo a instrução sql a ser
+        // executada, e nenhuma outra instução maliciosa estaria sendo considerada além daquela definida por nós.
 
-// QUERY E ROUTE PARAMS 
-    // A diferença fundamental está em como os parâmetros são passados na URL
-    // req.query.algo =>  aparecem após um ponto de interrogação (?) na URL, 
+// Params (query e route):
+    // São usadas para atribuir rotas personalizadas.
+        // A diferença fundamental está em como os parâmetros são passados na URL.
+
+        // req.query.algo =>  aparecem após um ponto de interrogação (?) na URL, 
         // usado para filtrar/personalizar solicitação de dados, opcionais
         app.get('/rota', (req, res) => {
-            const usuario = req.query.usuario;
-            const nome = req.query.nome;
-            // Restante do código...
-          });
+            const { atributo } = req.query; // req.query.algo ou a constante entre {} para não repetir duas vezes.
 
-    // req.params.algo => são incorporados à própria rota, usados para identificar recursos, obrigatórios
+            const INSTRUCAOSQL = 'SELECT * FROM posts WHERE id = ?';
+            bd.query(INSTRUCAOSQL, [atributo], (error, results) => {
+                if (error) {
+                    console.error(error);
+                }
+            })
+        });
+          // Exemplo de URL: http://exemplo.com/rota?usuario=valor1&nome=valor2
+
+        // req.params.algo => são incorporados à própria rota, usados para identificar recursos, obrigatórios
         app.get('/rota/:usuario/:nome', (req, res) => { // declarados após ':' 
             const usuario = req.params.usuario;
-            const nome = req.params.nome;
         });
+        // Exemplo de URL: http://exemplo.com/rota/usuario
+
+                                            
+// EXEMPLO DE CRUD (create, read, update, delete):
+
+    // GET (ler/aquirir):
+    server.get('/rotaDesejada', (req, res) => {
+        // req é de requisição recebida de outra aplicação
+        // res é de response que é a resposta enviada para outra aplicação
+        res.send('Hello world!');
+            // send para enviar dado.
+    })
+
+    // POST (enviar dados):
+    server.post('/rotaDesejada', (req, res)=> {
+        const {nome} = req.body;
+        const {email} = req.body;
+        const {numero} = req.body;
+        // isso permite que você extraia propriedades específicas de um objeto e as atribua a variáveis locais.
+        // o front-end envia esses atributos por seu método POST e é possível inserí-lo a partir do SQL no banco:
+
+        let SQL = "'INSERT INTO NomeDaTabela (nome, email, numero) VALUES (?, ?, ?)';"
+        // db.query para fazer uma solicitação ao banco:
+        // (primeiro o codigo sql, [parêmetros], depois a função (error, results))
+        db.query(SQL, [nome, email, numero], (error, results) => {
+            if(error) {
+                res.send(error); // envia para o front-end a mensagem de erro se a solicitação não deu certo.
+            }
+            // se não, é concluída com sucesso.
+        })
+    })
+    // PULL E DELETE, o mesmo se aplica a eles, só muda a declaração 'server.algo' e o código SQL:
+    server.pull('/rotaDesejada', (req, res)=> {
+        const {idDaPagina} = req.params;
+
+        const INSTRUCAOSQL = 'SELECT * FROM posts WHERE id = ?';
+        db.query(INSTRUCAOSQL, [idDaPagina], (error, results) => {
+            // algo a se fazer aqui
+        });
+    })
+    server.delete('/rotaDesejada', (req, res)=> {
+        const {idDaPagina} = req.params;
+
+        const INSTRUCAOSQL = 'DELETE FROM posts WHERE id = ?';
+        db.query(INSTRUCAOSQL, [idDaPagina], (error, results) => {
+            // algo a se fazer aqui
+        });
+    })
 
 
