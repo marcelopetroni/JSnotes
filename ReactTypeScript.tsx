b7121fab9f3f52d4f032d45a07584238f62a65a5
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-29 23:37:15 -0300

    funções

--- ReactTypeScript.tsx
+++ ReactTypeScript.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState} from 'react';
 // TYPESCRIPT
     // O TypeScript adiciona recursos úteis para a linguagem JavaScript, entre esses recursos, 
     // destacam-se a tipagem estática, interfaces, classes, herança, etc, + fácil de identificar bugs.
@@ -62,6 +64,65 @@ exemplo2(data.cpf!) // nesse exemplo eu tento chamar uma prop de tipo string | u
                     // e tem incompatibilidade com o parâmetro da função que é só string
 
 // solução? botar um '!' ao lado da prop que foi definida como opcional ou acrescentar '?' no param da função.
+
+
+// DIFERENÇA: entre type e interface:
+    // interface é mais indicado para definir estruturas de objetos/classes
+        // interfaces podem ser implementadas/extendidas
+
+    //  type é mais indicado para criar tipos de união, interseção, tipos complexos
+
+
+// USO DE FUNÇÕES com interface:
+    // os eventos das funções devem ter um tipo no TypeScript, veja no segundo exemplo a seguir.
+    // é possível ter como prop uma função dentro de uma interface, veja:
+
+    interface InformacoesUsuario {
+        cadastro() : string 
+        // nesse exemplo, têm-se uma função como parâmetro que necessita de um retorno de string
+    }
+    // em seguida, ao chamar a interface, é preciso declarar a função com o retorno do tipo desejado:
+    const exibirDadosUsuario: InformacoesUsuario = {
+        cadastro() {
+            return 'info pedida via string'
+            // aqui poderia também ter um retorno de um estado do tipo string onde já foi preenchido 
+            // o dado desejado anteriormente via forms
+        }
+    }
+
+    // Agora um uso prático de interface + função
+
+    interface Pessoa {
+        name: string
+        email: string
+    }
+    // declara o estado do tipo Pessoa (objeto)
+    const [dados, setDados] = useState/* <Pessoa> */({}); // 
+
+    // declara função que será extraído os dados em um forms no html
+    const handleSubmit = (e /*: React.FormEvent<HTMLFormElement>*/) => { // eventos precisam declar o tipo deles
+        const { name, email } = e.target;
+        setDados({
+            /*
+            [name]: value,
+            [email]: value */
+        })
+    }
+    // declara função que exibe dados do tipo desejado
+    const exibirData = (info:Pessoa) => {
+        // aqui foi criado uma arrow function com um parâmetro do tipo Pessoa (um objeto)
+        console.log(info.name); 
+        console.log(info.email);
+    }
+    // declara funçao que chamará ela
+    const handleData = (e /*:React.MouseEvent<HTMLButtonElement>*/) => {
+        e.preventDeafault();
+        exibirData(dados) // nesse exemplo um pouco incompleto, é exibido os dados através de um estado 
+                          // do tipo pessoa (objeto) 
+    }
+
+    // componente no html que chamará função que exibe dados.
+    < GamepadButton onClick = {handleData} /> // ao apertar o botão, é exibido os dados desejados.
 
 // UNION
     // Quando você define dois tipos para uma variável, nesse exemplo veremos de dois objetos:
